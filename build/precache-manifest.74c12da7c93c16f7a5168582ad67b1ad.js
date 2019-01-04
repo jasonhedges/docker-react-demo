6bed495264a9b178e4ed10b7d095f0ff94442a5f
Jason Hedges <jasonhedges@live.com>
2019-01-04 16:07:18 -0600

    Initial commit

---
+++ build/precache-manifest.74c12da7c93c16f7a5168582ad67b1ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "97e8c2ab4ef679579404",
+    "url": "/static/js/main.97e8c2ab.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "97e8c2ab4ef679579404",
+    "url": "/static/css/main.fc9b8fb7.chunk.css"
+  },
+  {
+    "revision": "829c95857c67156e0466ab9a35e9169f",
+    "url": "/index.html"
+  }
+];
